9be7a2d0a9b06b71b3ca566ac3c1257af46a3d1b
Irine Juliet <irenejuliet00@gmail.com>
2020-04-13 22:59:46 +0300

    add javascript to contact section

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,27 @@ $(document).ready(function() {
   $(".img").hover(function() {
     $(this).find(".projectName").toggle(400);
   });
+  $("form").submit(function() {
+    var name=document.getElementById('name').value;
+    var email= document.getElementById('email').value;
+    var message=document.getElementById('message').value;
+
+    if(name===""){
+     alert("Please fill all the fields");
+    }
+    else if(message===""){
+      alert("Please enter your message");
+      }
+    else if(email.length<10) {
+      alert("Please enter a valid email address");
+    }
+    else{
+      alert(" " + name + ", your message has been sent successfully. Thank you for reaching out to us.");
+    }
+  });
+  $("form#form").on ('submit',function() {
+    $('form').each(function(){
+    this.reset();
+  });
+}) ;
 });
